d58d73cf91d5d74bf3a4ceb66f69fc7e01cc0539
Larry Zhao <thehiddendepth@gmail.com>
2015-04-25 13:07:11 +0800

    fix key generation

--- lib/edward/value.rb
+++ lib/edward/value.rb
@@ -13,13 +13,13 @@ module Edward
       def value(name, options={})
         define_method(name) do
           Edward.ssdb.with do |conn|
-            conn.get("#{self.class.to_s.pluralize.downcase}:#{self.id}:#{name}") || options[:default]
+            conn.get("#{self.class.name.underscore.pluralize}:#{self.id}:#{name}") || options[:default]
           end
         end
 
         define_method("#{name}=") do |v|
           Edward.ssdb.with do |conn|
-            conn.set("#{self.class.to_s.pluralize.downcase}:#{self.id}:#{name}", v)
+            conn.set("#{self.class.name.underscore.pluralize}:#{self.id}:#{name}", v)
           end
         end
       end
